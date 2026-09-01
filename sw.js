a3438af8ede5f22193e4f6dfa4bff52c27232eaf
Sonali Garg <145942258+so-sonali@users.noreply.github.com>
2026-09-01 18:20:31 -0400

    Cache NOOR collaboration and audit modules

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='noor-v1.3.5-reference-fix';
-const ASSETS=['./','./index.html','./styles.css','./manifest.webmanifest','./js/app.js','./js/config.js','./js/storage.js','./js/tools.js','./js/media.js','./js/reference-enhancements.js','./js/formula-formatting.js','./js/postdoc-workflow.js','./js/handwriting.js','./js/project-linking.js'];
+const CACHE='noor-v2.0.0-collaboration';
+const ASSETS=['./','./index.html','./styles.css','./manifest.webmanifest','./js/app.js','./js/config.js','./js/storage.js','./js/tools.js','./js/media.js','./js/reference-enhancements.js','./js/formula-formatting.js','./js/postdoc-workflow.js','./js/handwriting.js','./js/project-linking.js','./js/audit-history.js','./js/cloud-sync.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{
