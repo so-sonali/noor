0b002a9876c968d53917876e5b0e57e5b83b26e3
Sonali Garg <145942258+so-sonali@users.noreply.github.com>
2026-09-01 12:16:05 -0400

    Refresh cache for Reference Hub fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='noor-v1.3.4-project-picker';
+const CACHE='noor-v1.3.5-reference-fix';
 const ASSETS=['./','./index.html','./styles.css','./manifest.webmanifest','./js/app.js','./js/config.js','./js/storage.js','./js/tools.js','./js/media.js','./js/reference-enhancements.js','./js/formula-formatting.js','./js/postdoc-workflow.js','./js/handwriting.js','./js/project-linking.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
