ae9f24e7f9dbddfaa33ce8fea615c3f1e7060a51
Sonali Garg <145942258+so-sonali@users.noreply.github.com>
2026-09-01 08:02:44 -0400

    Cache intensive reference hub

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='noor-v1.1.0';
-const ASSETS=['./','./index.html','./styles.css','./manifest.webmanifest','./js/app.js','./js/config.js','./js/storage.js','./js/tools.js','./js/media.js'];
+const CACHE='noor-v1.1.1-reference';
+const ASSETS=['./','./index.html','./styles.css','./manifest.webmanifest','./js/app.js','./js/config.js','./js/storage.js','./js/tools.js','./js/media.js','./js/reference-enhancements.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{
