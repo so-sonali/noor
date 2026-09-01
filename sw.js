170ea1656075982439992ff5c9189aa40726303c
Sonali Garg <145942258+so-sonali@users.noreply.github.com>
2026-09-01 11:55:23 -0400

    Deploy custom experiment type update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='noor-v1.3.1-custom-type';
+const CACHE='noor-v1.3.1-custom-type-r1';
 const ASSETS=['./','./index.html','./styles.css','./manifest.webmanifest','./js/app.js','./js/config.js','./js/storage.js','./js/tools.js','./js/media.js','./js/reference-enhancements.js','./js/formula-formatting.js','./js/postdoc-workflow.js','./js/handwriting.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
