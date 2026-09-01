0582d4978132a65b79da072c0b78e7ffc6190b6c
Sonali Garg <145942258+so-sonali@users.noreply.github.com>
2026-09-01 07:46:34 -0400

    Cache camera and media module offline

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='noor-v1.0.1';
-const ASSETS=['./','./index.html','./styles.css','./manifest.webmanifest','./js/app.js','./js/config.js','./js/storage.js','./js/tools.js'];
+const CACHE='noor-v1.1.0';
+const ASSETS=['./','./index.html','./styles.css','./manifest.webmanifest','./js/app.js','./js/config.js','./js/storage.js','./js/tools.js','./js/media.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{
